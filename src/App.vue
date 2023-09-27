cd92ca00f2b1e36e2b86ac38cd5a9fb2633ef883
shuyj <281660848@qq.com>
2023-09-27 21:33:06 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
@@ -8,23 +9,22 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #000000;
 }
 
 nav {
+  background-color: #333;
+  color: #fff;
+  font-size: 24px;
   padding: 30px;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #0088fa;
 }
 </style>
