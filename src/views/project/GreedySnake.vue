0b050b62d561e74e0909981468307ac8d2cc8636
shuyj <281660848@qq.com>
2023-09-29 12:30:17 +0800

    feat(project): init greedy snake

--- src/views/project/GreedySnake.vue
+++ src/views/project/GreedySnake.vue
@@ -1,145 +1,138 @@
 <template>
-  <div id="game-board"></div>
+  <button @click="restartGame">Start</button>
+  <div>score: {{ snakeLength - 1 }}</div>
+  <div id="game-board">
+    <div v-for="segment in snake" :key="segment.id" class="snake"
+         :style="{ left: segment.x * 20 + 'px', top: segment.y * 20 + 'px' }"></div>
+    <div v-if="food" class="food" :style="{ left: food.x * 20 + 'px', top: food.y * 20 + 'px' }"></div>
+  </div>
+  <div class="direction-buttons">
+    <div class="vertical-buttons">
+      <button @click="move('up')">Up</button>
+      <div class="horizontal-buttons">
+        <button @click="move('left')">Left</button>
+        <button @click="move('right')">Right</button>
+      </div>
+      <button @click="move('down')">Down</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "GreedySnake"
-}
-// 获取游戏面板元素
-const gameBoard = document.getElementById('game-board');
+  name: "GreedySnake",
+  data() {
+    return {
+      direction: 'left',
+      snake: [{x: 10, y: 10}],
+      snakeLength: 1,
+      food: '',
+    };
+  },
+  mounted() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  },
+  methods: {
+    move(direction) {
+      this.direction = direction;
+    },
 
-// 定义蛇初始位置和长度
-let snake = [{x: 10, y: 10}];
-let snakeLength = 1;
+    handleKeyDown(event) {
+      if (event.key === "ArrowUp" && this.direction !== "down") {
+        this.direction = "up";
+      } else if (event.key === "ArrowDown" && this.direction !== "up") {
+        this.direction = "down";
+      } else if (event.key === "ArrowLeft" && this.direction !== "right") {
+        this.direction = "left";
+      } else if (event.key === "ArrowRight" && this.direction !== "left") {
+        this.direction = "right";
+      }
+    },
+    updateGame() {
+      const head = {x: this.snake[0].x + this.getDirectionX(), y: this.snake[0].y + this.getDirectionY()};
+      this.snake.unshift(head);
+      if (head.x === this.food.x && head.y === this.food.y) {
+        this.snakeLength++;
+        this.food = this.generateFood();
+      }
+      if (this.snake.length > this.snakeLength) {
+        this.snake.pop();
+      }
+      if (this.isSnakeDead()) {
+        clearInterval(this.gameInterval);
+        alert('游戏结束！');
+        this.snake = [{x: 10, y: 10}];
+        this.food = '';
+      }
+    },
 
-// 定义食物的初始位置
-let food = {x: 5, y: 5};
-
-// 定义当前移动方向
-let direction = 'right';
-
-// 监听键盘按键事件来改变蛇的移动方向
-document.addEventListener('keydown', changeDirection);
-
-// 改变蛇的移动方向
-function changeDirection(event) {
-  if (event.keyCode === 37 && direction !== 'right') {
-    direction = 'left';
-  } else if (event.keyCode === 38 && direction !== 'down') {
-    direction = 'up';
-  } else if (event.keyCode === 39 && direction !== 'left') {
-    direction = 'right';
-  } else if (event.keyCode === 40 && direction !== 'up') {
-    direction = 'down';
+    getDirectionX() {
+      if (this.direction === 'left') {
+        return -1;
+      } else if (this.direction === 'right') {
+        return 1;
+      } else {
+        return 0;
+      }
+    },
+    getDirectionY() {
+      if (this.direction === 'up') {
+        return -1;
+      } else if (this.direction === 'down') {
+        return 1;
+      } else {
+        return 0;
+      }
+    },
+    isSnakeDead() {
+      const head = this.snake[0];
+      // 检查是否触碰边界
+      if (head.x < 0 || head.x > 20 || head.y < 0 || head.y > 20) {
+        return true;
+      }
+      // 检查是否吃到自己
+      for (let i = 1; i < this.snake.length; i++) {
+        if (head.x === this.snake[i].x && head.y === this.snake[i].y) {
+          return true;
+        }
+      }
+      return false;
+    },
+    generateFood() {
+      while (1) {
+        let x = Math.floor(Math.random() * 20);
+        let y = Math.floor(Math.random() * 20);
+        let flag = false;
+        for (let segment in this.snake) {
+          if (x === segment.x && y === segment.y) {
+            flag = true;
+            break;
+          }
+        }
+        if (!flag) {
+          break;
+        }
+      }
+      return {
+        x: Math.floor(Math.random() * 20),
+        y: Math.floor(Math.random() * 20)
+      };
+    },
+    restartGame() {
+      this.snake = [{x: 10, y: 10}];
+      this.snakeLength = 1;
+      this.food = this.generateFood();
+      this.direction = 'right';
+      this.gameInterval = setInterval(this.updateGame, 250);
+    },
   }
 }
-
-// 更新游戏状态
-function updateGame() {
-  // 清空游戏面板
-  gameBoard.innerHTML = '';
-
-  // 移动蛇的身体
-  const head = {x: snake[0].x + getDirectionX(), y: snake[0].y + getDirectionY()};
-  snake.unshift(head);
-  if (snake.length > snakeLength) {
-    snake.pop();
-  }
-
-  // 检查是否蛇吃到了自己或触碰到边界
-  if (isSnakeDead()) {
-    clearInterval(gameInterval);
-    alert('游戏结束！');
-    restartGame();
-    return;
-  }
-
-  // 检查是否吃到食物
-  if (head.x === food.x && head.y === food.y) {
-    snakeLength++;
-    food = generateFood();
-  }
-
-  // 绘制蛇
-  snake.forEach(segment => {
-    const snakeElement = document.createElement('div');
-    snakeElement.style.left = segment.x * 20 + 'px';
-    snakeElement.style.top = segment.y * 20 + 'px';
-    snakeElement.classList.add('snake');
-    gameBoard.appendChild(snakeElement);
-  });
-
-  // 绘制食物
-  const foodElement = document.createElement('div');
-  foodElement.style.left = food.x * 20 + 'px';
-  foodElement.style.top = food.y * 20 + 'px';
-  foodElement.classList.add('food');
-  gameBoard.appendChild(foodElement);
-}
-
-// 根据当前方向获取移动偏移量
-function getDirectionX() {
-  if (direction === 'left') {
-    return -1;
-  } else if (direction === 'right') {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-function getDirectionY() {
-  if (direction === 'up') {
-    return -1;
-  } else if (direction === 'down') {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-// 检测蛇是否死亡
-function isSnakeDead() {
-  const head = snake[0];
-  // 检查是否触碰边界
-  if (head.x < 0 || head.x >= 20 || head.y < 0 || head.y >= 20) {
-    return true;
-  }
-  // 检查是否吃到自己
-  for (let i = 1; i < snake.length; i++) {
-    if (head.x === snake[i].x && head.y === snake[i].y) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// 生成随机食物位置
-function generateFood() {
-  return {
-    x: Math.floor(Math.random() * 20),
-    y: Math.floor(Math.random() * 20)
-  };
-}
-
-// 重新开始游戏
-function restartGame() {
-  snake = [{x: 10, y: 10}];
-  snakeLength = 1;
-  food = generateFood();
-  direction = 'right';
-  //gameInterval = setInterval(updateGame, 250);
-}
-
-// 游戏循环
-//let gameInterval = setInterval(updateGame, 250);
 </script>
 
 <style scoped>
 #game-board {
-  width: 400px;
-  height: 400px;
+  width: 420px;
+  height: 420px;
   border: 2px solid #000;
   position: relative;
   margin: 0 auto;
@@ -157,5 +150,37 @@ function restartGame() {
   height: 20px;
   background-color: red;
   position: absolute;
+}
+
+.direction-buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.vertical-buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.horizontal-buttons {
+  display: flex;
+  justify-content: center;
+}
+
+button {
+  margin: 10px;
+  padding: 10px 20px;
+  font-size: 18px;
+  border: none;
+  background-color: #ddd;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #ccc;
 }
 </style>
